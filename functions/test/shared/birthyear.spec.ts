2136810c370bb8d97ac961ae7691fe53c8e861e9
Daniel Zeiter <daniel.zeiter@ergon.ch>
2019-07-09 16:03:07 +0200

    Add unit test config

---
+++ functions/test/shared/birthyear.spec.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+// Assertion library.
+import { use } from 'chai';
+// Extends Chai with assertions for the Sinon.JS 
+import * as sinonChai from 'sinon-chai';
+
+use(sinonChai);
+
+describe('birthyear', () => {
+
+    it('50 year old should be born about 1969', () => {
+        // TODO implement
+    });
+
+    it('1 year old should be born about 2018', () => {
+        // TODO implement
+    });
+
+});
